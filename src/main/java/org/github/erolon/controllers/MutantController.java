0a25f8bd3dd71593164616bb7379d3f08569dd87
erolon <esegundorolon@prismamp.com>
2018-01-28 12:43:27 -0300

    Agrego logs

--- src/main/java/org/github/erolon/controllers/MutantController.java
+++ src/main/java/org/github/erolon/controllers/MutantController.java
@@ -6,6 +6,9 @@ import org.github.erolon.dto.DNASequenceRequest;
 import org.github.erolon.model.DNASequence;
 import org.github.erolon.service.IDNAValidateService;
 import org.github.erolon.service.IMutantValidateService;
+import org.github.erolon.service.Impl.MutantValidateServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -14,8 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 public class MutantController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MutantController.class);
 	
 	@Autowired
 	IMutantValidateService mutantService;
@@ -25,7 +33,9 @@ public class MutantController {
 	
 
 	@RequestMapping(value = "mutant", method = RequestMethod.POST)
-	public Object mutantDetector(@RequestBody @Valid DNASequenceRequest request, BindingResult result) throws BindException {
+	public Object mutantDetector(@RequestBody @Valid DNASequenceRequest request, BindingResult result) throws BindException, JsonProcessingException {
+		LOGGER.info("Comienza servicio de verificacion de mutantes");
+		 LOGGER.info("Request recibido: " + new ObjectMapper().writeValueAsString(request));
 		return mutantService.verifyMutation(new DNASequence(request));
 	}
 }
--- src/main/java/org/github/erolon/service/Impl/MutantValidateServiceImpl.java
+++ src/main/java/org/github/erolon/service/Impl/MutantValidateServiceImpl.java
@@ -10,12 +10,18 @@ import org.github.erolon.model.Matrix;
 import org.github.erolon.repository.IDNASequenceRepository;
 import org.github.erolon.service.IMutantValidateService;
 import org.github.erolon.utils.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.prisma.todopago.repository.transactions.bsa.impl.TrxEventsRepositoryImpl;
+
 @Service
 public class MutantValidateServiceImpl  implements IMutantValidateService{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MutantValidateServiceImpl.class);
 	
 	@Value("${mutant.match.factor}")
 	private String numberOfNitrogenBasesRepetition;
@@ -28,14 +34,14 @@ public class MutantValidateServiceImpl  implements IMutantValidateService{
 	
 	@Override
 	public boolean verifyMutation(DNASequence dnaSequence) {
-		
+		LOGGER.info("Analizando si secuencia recibida es mutante");
 		if(!isMutant(dnaSequence))
 			throw new NonMutantException();
-		
+		LOGGER.info("La secuencia recibida es mutante, guardando en datastore");
 		dnaSequence.setIsMutant(true);
 		dnaSequenceRepository.saveDNASequence(dnaSequence);
-		System.out.print("Registros: "+dnaSequenceRepository.countAllDNASequences());
-		System.out.print("Registros Mutantes: "+dnaSequenceRepository.countAllMutantDNASequences());
+		LOGGER.info("En total se tienen :"+dnaSequenceRepository.countAllDNASequences());
+		LOGGER.info("Los mutantes son :"+dnaSequenceRepository.countAllMutantDNASequences());
 		return true;
 	}
 	
@@ -44,10 +50,11 @@ public class MutantValidateServiceImpl  implements IMutantValidateService{
 		int numberOfSequencesOccurence = Integer.parseInt(this.numberOfSequencesOccurence);
 		
 		int horizontalFinded = dnaSequence.repeatedRowWithNitrogenBasesHorizontally( numberOfNitrogenBasesRepetition , numberOfSequencesOccurence );
-		System.out.println("DNA: "+dnaSequence.getDNA()+" found: "+horizontalFinded);
+		LOGGER.info("Horizontalmente se encontraron : "+horizontalFinded+" letras iguales");
 		if( horizontalFinded >= numberOfSequencesOccurence)			
 			return true;
 		int verticalFinded = dnaSequence.repeatedColumnsWithNitrogenBasesVertically( numberOfNitrogenBasesRepetition , numberOfSequencesOccurence);
+		LOGGER.info("Verticalmente se encontraron : "+verticalFinded+" letras iguales");
 		if( (verticalFinded+horizontalFinded) >= numberOfSequencesOccurence)			
 			return true;
 		int obliquelyFinded = dnaSequence.repeatedColumnsWithNitrogenBasesObliquely( numberOfNitrogenBasesRepetition , numberOfSequencesOccurence);
