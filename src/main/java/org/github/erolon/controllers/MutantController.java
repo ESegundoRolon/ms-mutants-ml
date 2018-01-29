759d6b557cc40a853a811b83a9ca4e68698efc45
erolon <esegundorolon@prismamp.com>
2018-01-28 21:43:26 -0300

    Agrego handleo de errores y respuestas custom para cada exception

--- src/main/java/org/github/erolon/controllers/MutantController.java
+++ src/main/java/org/github/erolon/controllers/MutantController.java
@@ -3,13 +3,19 @@ package org.github.erolon.controllers;
 import javax.validation.Valid;
 
 import org.github.erolon.dto.DNASequenceRequest;
+import org.github.erolon.errors.ErrorHandler;
+import org.github.erolon.exceptions.InvalidDNASequenceException;
+import org.github.erolon.exceptions.NonMutantException;
 import org.github.erolon.model.DNASequence;
 import org.github.erolon.service.IDNAValidateService;
-import org.github.erolon.service.IMutantValidateService;
-import org.github.erolon.service.Impl.MutantValidateServiceImpl;
+import org.github.erolon.service.IMutantDetectorService;
+import org.github.erolon.service.IStatsService;
+import org.github.erolon.service.Impl.MutantDetectorServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,16 +32,60 @@ public class MutantController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MutantController.class);
 	
 	@Autowired
-	IMutantValidateService mutantService;
-	
-	@Autowired
 	IDNAValidateService mutantValidationService;
 	
+	@Autowired
+	IMutantDetectorService mutantService;
+	
+	@Autowired
+	IStatsService statsService;
+	
+	@Autowired
+	ErrorHandler errorHandler;
 
+	  /**
+	   *Endpoint que recibe secuencia de ADN y devuelve si es mutante o no
+	   *
+	   */
 	@RequestMapping(value = "mutant", method = RequestMethod.POST)
-	public Object mutantDetector(@RequestBody @Valid DNASequenceRequest request, BindingResult result) throws BindException, JsonProcessingException {
+	public Object mutantDetector(@RequestBody @Valid DNASequenceRequest request, BindingResult result) throws BindException {
+		if(result.hasErrors())
+			return errorHandler.handleValidationError(result);
 		LOGGER.info("Comienza servicio de verificacion de mutantes");
-		 LOGGER.info("Request recibido: " + new ObjectMapper().writeValueAsString(request));
-		return mutantService.verifyMutation(new DNASequence(request));
+		DNASequence dnaSequence = new DNASequence(request);
+		
+		try{
+			mutantValidationService.isValidOrThrowException(dnaSequence);
+			return new ResponseEntity<>( mutantService.verifyMutation(dnaSequence) , HttpStatus.OK );
+		}catch(InvalidDNASequenceException idsex){
+			return errorHandler.handleInvalidDNASequenceError(idsex.getMessage());
+		}catch(NonMutantException nmex){
+			return errorHandler.handleNonMutantError(nmex.getMessage());
+		}catch(Exception ex){
+			return errorHandler.handleGenericError();
+		}		
 	}
+	
+	  /**
+	   *Endpoint obtener estadisticas de las secuencias de ADN guardadas
+	   *
+	   */
+	@RequestMapping(value = "stats", method = RequestMethod.GET)
+	public Object stats() {
+		try {
+			return new ResponseEntity<>( statsService.getStats() , HttpStatus.OK );
+		}catch(Exception ex){
+			return errorHandler.handleGenericError();
+		}
+	}
+	
+	  /**
+	   *Endpoint para healtcheck de Google Cloud para App Engine
+	   *
+	   */
+	  @RequestMapping("/_ah/health")
+	  public String healthy() {
+	    // Message body required though ignored
+	    return " OK ";
+	  }
 }
---
+++ src/main/java/org/github/erolon/errors/ErrorHandler.java
@@ -0,0 +1,66 @@
+package org.github.erolon.errors;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.github.erolon.dto.GenericResponse;
+import org.github.erolon.model.DNASequence;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+@Component
+public class ErrorHandler {
+	
+	private static final String UNKNOWN_ERROR_MESSAGE = "Ha ocurrido un error inesperado";
+	private static final String BAD_REQUEST_MESSAGE = "El body recibido es invalido, se espera un array de Strings";
+	@Value("${ms.configuration.dateFormat}")
+	private String dateFormat;
+	
+	public ResponseEntity<GenericResponse> handleValidationError(BindingResult bindingResult){
+		
+		GenericResponse response = new GenericResponse();
+		response.setMessage(BAD_REQUEST_MESSAGE);
+		response.setField(bindingResult.getFieldError().getDefaultMessage());
+		SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+		response.setTimestamp(format.format(new Date()));
+		
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+	
+	public ResponseEntity<GenericResponse> handleInvalidDNASequenceError(String message){
+		
+		GenericResponse response = new GenericResponse();
+		response.setMessage(message);
+		SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+		response.setTimestamp(format.format(new Date()));
+		
+		return new ResponseEntity<>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+
+	
+	public ResponseEntity<GenericResponse> handleGenericError(){
+		
+		GenericResponse response = new GenericResponse();
+		response.setMessage(UNKNOWN_ERROR_MESSAGE);
+		SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+		response.setTimestamp(format.format(new Date()));
+		
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	public ResponseEntity<GenericResponse> handleNonMutantError(String message){
+		
+		GenericResponse response = new GenericResponse();
+		response.setMessage(message);
+		SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+		response.setTimestamp(format.format(new Date()));
+		
+		return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+
+	}
+
+
+}
